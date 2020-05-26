2fc7f42f205fcd69de9082110ba29e92118af605
Constantin Dimitrenco <56898295+cockochan@users.noreply.github.com>
2020-05-26 19:45:23 +0100

    hmm....

---
+++ src/cardContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import customData from './data.json/';
+const CardContainer =(customData)=> {
+console.log(customData)
+    return(
+        <div>CardContainer</div>
+    )
+
+}
+export default CardContainer
