8226813d474a83005e50c8ce2759bb1817e8643f
Constantin Dimitrenco <56898295+cockochan@users.noreply.github.com>
2020-05-30 14:40:44 +0100

    sergserh

--- src/Card.js
+++ src/Card.js
@@ -1,47 +1,47 @@
 import React from 'react';
 import Sqwaretag from './Sqwaretag'
-const Card =(props)=>{
+const Card = (props) => {
     let source = props.unemployed.logo;
     console.log(source)
-return(
-    <div key = {props.unemployed.id} className='Card row'>
+    return (
+        <div key={props.unemployed.id} className='Card row'>
 
-        <img src ={source} className = "logo col-4" alt ='nothing to see here anyway' ></img>
-        
-        <div className="aboutBlock col-5">
-            <div className='tagsNew'>
-            <span className='companyName'>
-    <div > 
-    </div>
-    <div>
-    {(props.unemployed.new)?(<div className ='Sqwaretag'>new </div>):null}
-    </div>
-    <div>
-   <div className ='Sqwaretag'>{props.unemployed.role} </div>
-    </div>
-            {props.unemployed.company}
-            </span>
-            </div>
-            <div className='labels'>
-            </div>
-            <p className ='position'>
-                {props.unemployed.position}
-            </p>
-            
-            <p className = 'descriptiveTags row'>
-                <span className = 'contract  nowrap'> {props.unemployed.contract }&#9679;</span>
-                <span className = 'posted nowrap'> {props.unemployed.postedAt }&#9679;	</span>
-                <span className = 'location  nowrap'> {props.unemployed.location}</span>
-            </p>
-            </div>
-            <div className ='tagContainer col-4'>
-                <Sqwaretag unemployed={props.unemployed}></Sqwaretag>
+            <img src={source} className="logo col-4" alt='nothing to see here anyway' ></img>
+
+            <div className="aboutBlock col-5">
+                <div className='tagsNew'>
+                    <span className='companyName'>
+                        <div >
+                        </div>
+                        <div>
+                            {(props.unemployed.new) ? (<div className='Sqwaretag'>new </div>) : null}
+                        </div>
+                        <div>
+                            <div className='Sqwaretag'>{props.unemployed.role} </div>
+                        </div>
+                        {props.unemployed.company}
+                    </span>
                 </div>
-        
-        
-    
-    
-    </div>
-)
+                <div className='labels'>
+                </div>
+                <p className='position'>
+                    {props.unemployed.position}
+                </p>
+
+                <p className='descriptiveTags row'>
+                    <span className='contract  nowrap'> {props.unemployed.contract}&#9679;</span>
+                    <span className='posted nowrap'> {props.unemployed.postedAt}&#9679;	</span>
+                    <span className='location  nowrap'> {props.unemployed.location}</span>
+                </p>
+            </div>
+            <div className='tagContainer col-4'>
+                <Sqwaretag unemployed={props.unemployed}></Sqwaretag>
+            </div>
+
+
+
+
+        </div>
+    )
 }
 export default Card
