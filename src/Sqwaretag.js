cfa30c7e92bd268fec241154743adbcfd3991267
Constantin Dimitrenco <56898295+cockochan@users.noreply.github.com>
2020-05-30 19:26:46 +0100

    Revert "Merge branch 'master' of https://github.com/Cockochan/job-listings-react"
    This reverts commit 86027e48e92042d62bf4028914258599f2e451f8, reversing
    changes made to b242ab91df839cca60c998bb3e052261205d5009.

--- src/Sqwaretag.js
+++ src/Sqwaretag.js
@@ -10,12 +10,9 @@ const Sqwaretag = (props)=>{
             <div className="Sqwaretag" key={props.unemployed.id}>
                 {language}
             </div>   
-    )},
-     
+    )}
   
     )}
-    <div className="Sqwaretag">{props.unemployed.level}</div>
-    <div className="Sqwaretag">{props.unemployed.role}</div>
     </div>
     )}
     
